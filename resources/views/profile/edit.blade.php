2f29b7f3e99bae78e42925fa0204b454d753d6de
Edson Gabriel <egmoreira10@gmail.com>
2024-05-02 03:08:15 -0300

    feat: Add responsiveness to dashboard page

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout padding='false'>
     <x-slot name="slot">
-        <div class="flex">
-            <aside class="w-1/4 bg-white flex flex-col py-5">
+        <div class="flex flex-col md:flex-row">
+            <aside class="hidden md:flex md:flex-col w-1/4 bg-white py-5">
                 <a href="{{ route('profile.edit', ['tab' => 'info']) }}"
                     class="p-4 text-start font-bold border-b bg-white hover:brightness-95 active:brightness-90 transition-all">{{ __('Profile Information') }}</a>
 
@@ -9,7 +9,17 @@
                     class="p-4 text-start font-bold border-b bg-white hover:brightness-95 active:brightness-90 transition-all">{{ __('Posts') }}</a>
             </aside>
 
-            <div class="w-3/4 bg-white">
+            <menu class="md:hidden px-5 mb-2">
+                <select name="tab" id="tab" class="w-full p-2 border rounded-md"
+                    onchange="window.location.href = this.value">
+                    <option value="{{ route('profile.edit', ['tab' => 'info']) }}" @if(!isset($tab) || $tab==='info' )
+                        selected @endif>{{ __('Profile Information') }}</option>
+                    <option value="{{ route('profile.edit', ['tab' => 'posts']) }}" @if($tab==='posts' ) selected
+                        @endif>{{ __('Posts') }}</option>
+                </select>
+            </menu>
+
+            <div class="w-full md:w-3/4 bg-white">
                 <div class="h-[calc(100dvh-85px)] mb-5 bg-gray-50 rounded-s-3xl border overflow-y-scroll p-5">
                     @if(!isset($tab) || $tab === 'info')
                     @include('profile.partials.info')
--- resources/views/profile/partials/posts.blade.php
+++ resources/views/profile/partials/posts.blade.php
@@ -6,12 +6,12 @@
             $imageUrl = 'storage/' . $post->image;
         ?>
 
-        <li class="flex justify-between gap-3 rounded-xl w-full text-start transition-all">
-            <div class="w-2/12 h-32 overflow-hidden rounded-md">
+        <li class="flex flex-col md:flex-row justify-between gap-3 rounded-xl w-full text-start transition-all">
+            <div class="w-full md:w-2/12 h-32 overflow-hidden rounded-md">
                 <img class="w-full h-full object-cover" src="{{ asset($imageUrl) }}" alt="">
             </div>
 
-            <div class="w-9/12">
+            <div class="w-full md:w-9/12">
                 <a href="{{ route('posts.show', $post) }}">
                     <h3>{{ $post->title }}</h3>
                 </a>
@@ -20,7 +20,7 @@
                 <p class="text-sm">{{ __('Updated') }}: {{ $post->updated_at->diffForHumans() }}</p>
             </div>
 
-            <div class="w-1/12 flex flex-col items-end justify-between">
+            <div class="w-full md:w-1/12 flex md:flex-col items-end justify-between">
                 <a href="{{ route('posts.edit', ['post' => $post]) }}">
                     <img src="{{ asset('icons/edit.svg') }}" alt=""
                         class="w-7 md:w-9 opacity-50 rounded-lg p-1 hover:opacity-100 hover:bg-gray-300 active:brightness-95 transition-all">
@@ -34,18 +34,18 @@
 
                     <x-slot name="content">
                         <form method="POST" action="{{ route('posts.destroy', ['post' => $post]) }}"
-                            class="flex flex-col gap-1">
+                            class="flex flex-col">
                             @csrf
                             @method('DELETE')
 
                             <p class="p-1 text-center font-bold">{{ __('Are you sure you want to delete this post?') }}
                             </p>
 
-                            <x-danger-button type="submit" class="w-full">
+                            <x-danger-button type="submit" class="w-full rounded-none">
                                 {{ __('Delete') }}
                             </x-danger-button>
 
-                            <x-secondary-button type="button" class="w-full">
+                            <x-secondary-button type="button" class="w-full rounded-none">
                                 {{ __('Cancel') }}
                             </x-secondary-button>
                         </form>
